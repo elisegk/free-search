6ec9709ebb34d08adf9ec6782b401f8c68962f15
elisegk <za.king17@gmail.com>
2024-05-19 00:24:25 +0000

    added websites

--- robots/Sydney.py
+++ robots/Sydney.py
@@ -2,3 +2,13 @@ import scrapy
 
 print("Done importing scrapy")
 
+websites = [
+    "https://en.wikipedia.org/wiki/Axolotl",
+    "https://kidsgardening.org/*",
+    "https://kids.nationalgeographic.com/*",
+    "https://kids.kiddle.co/*",
+    "https://kids.britannica.com/*",
+    
+
+
+]
